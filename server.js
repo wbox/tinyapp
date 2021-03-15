6729630d92367ba302f1e6fceba91d993add27be
Sam <sribas@gmail.com>
2021-03-15 14:31:34 +0000

    Passing data to views and partials

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.set('view engine', 'ejs');
 
 // index.page
 app.get('/', (req, res) =>{
-  res.render('pages/index');
+  let mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  let tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
 // about page
