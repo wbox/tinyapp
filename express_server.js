3b55bfa84544ff5f4e5c5a8427f0840192fbf4a3
Sam <sribas@gmail.com>
2021-03-18 20:23:19 +0000

    Week3/Day4 - Fixing some details after running eslint

--- express_server.js
+++ express_server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-const md5 = require('md5');
 const ejs = require('ejs');
-const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
+const md5 = require('md5');
+//const bcrypt = require('bcrypt');
+//const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const { generateRandomString, findUserByEmail, findUserById, addNewUser, validateUser, getUserUrls } = require('./helpers');
 
@@ -17,19 +17,16 @@ app.use(
   cookieSession({
     name: 'session',
     keys: ['key1', 'key2']
-    })
+  })
 );
 
 // Middleware functions
 const setCurrentUser = (req, res, next) => {
-
   const userID = req.session['user_id'];
   const userObj = users[userID] || null;
   req.currentUser = userObj;
-
-  console.log("req.currentUser from setCurrentUser middleware function:",req.currentUser);
   next();
-}
+};
 
 app.use(setCurrentUser);
 
@@ -48,15 +45,15 @@ const urlDatabase = {
   i3BoGr: { longURL: "https://www.google.ca", userID: "userRandomID" }
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   },
   '80e100': {
@@ -65,100 +62,6 @@ const users = {
     password: '$2b$10$2dA3e5.e3H51UTayA30qruiQrNw73lBfq474YfiTltNLPinfzWWVq'
   }
 };
-
-// /// Helper Functions
-// const generateRandomString = (text, length) => {
-//   length = !length ? 6 : length;
-//   const randomString = md5(text).slice(0,length);
-//   return randomString;
-// };
-
-// const findUserById = (id, users) => {
-//     const userDB = Object.values(users).find(userObject => userObject.id === id);
-
-//     console.log("--->userDB inside findUserById:", userDB);
-//     console.log("--->id inside findUserById:", id);
-//     console.log("--->users inside findUserById:", users);
-
-//     if (userDB) {
-//       return { userDB, error: null };
-//     } else {
-//       // const user = null;
-//       const userDB = null;
-//       return { userDB, error: "User ID doesn't exist"}
-//     }
-
-// };
-
-// const findUserByEmail = (email, userDB) => {
-//   const user = Object.values(userDB).find((userObject) => userObject.email === email);
-//   if (user) {
-//     return { user, error: null };
-//   } else {
-//     const user = null;
-//     return { user, error: "User email doesn't exist"}
-//   }
-// };
-
-// function addNewUser(id, email, password, users) {
-//   password = bcrypt.hashSync(password, SALT_ROUND);
-//   users[id] = { id, email, password };
-//   const userDB = users[id];
-//   return { userDB , error: null };
-// };
-
-// const validateUser = (email, password, users) => {
-//   userDB = null;
-//   if (!email) {
-//     return { userDB , error: "email empty" };
-//   } else if (!password) {
-//     return { userDB, error: "password empty" };
-//   } else if (!users) {
-//     return { userDB, error: "database empty"} ;
-//   };
-
-//   userDB = Object.values(users).find(objectUser => objectUser.email === email);
-  
-//   console.log("---->userDB inside login:", userDB);
-
-//   if (!userDB) {
-//     return { userDB , error: "User not found" };
-//   } else { 
-  
-//     const hash = userDB.password;
-    
-//     console.log("hash inside validateUser Function:", hash);
-//     console.log("compareSync:", bcrypt.compareSync(password, hash));
-
-//     if (userDB.email !== email) {
-//       return { userDB, error: "email not found!" };
-//     } else if (!bcrypt.compareSync(password, hash)) /* userFromDb.password !== password) */{
-//       return { userDB, error: "wrong password" }; 
-//     } 
-//     return { userDB, error: null };
-//   }
-// };
-
-// const getUserUrls = (id, urlDatabase) => {
-//   const userUrlObj = {};
-
-//   console.log("---> urlDatabase:",urlDatabase)
-
-//   for (let url in urlDatabase) {
-//     console.log("url in the loop:", url)
-//     console.log("urlDatabase[url] in the loop:", urlDatabase[url]);
-//     console.log("urlDatabase[url].userID in the loop:", urlDatabase[url].userID);
-//     console.log("userDB.id in the loop:", id);
-
-//     if ( urlDatabase[url].userID == id) {
-//       console.log("--->", url);
-      
-//       userUrlObj[url] =  urlDatabase[url];
-//     }
-//   }
-//   console.log(userUrlObj);
-//   return userUrlObj || null;
-// }
 
 // POST Routing Entries
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -169,10 +72,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL/edit", (req, res) => {
   const { userDB, error } = findUserById(req.session.user_id, users);
   if (userDB) {
-
-    console.log("req.params inside edit:",req.params);
-    console.log("urlDatabase:",urlDatabase);
-
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, userDB };
     res.render("urls_show", templateVars);
   }
@@ -186,7 +85,6 @@ app.post("/urls/:id", (req, res) => {
 app.post("/urls", (req, res) => {
   shortURLKey = generateRandomString(req.body.longURL, SHORTURL_LENGTH);
   urlDatabase[shortURLKey] = { longURL: req.body.longURL, userID: req.session.user_id };
-  console.log(urlDatabase);
   res.redirect('/urls');
 });
 
@@ -200,24 +98,12 @@ app.post("/login", (req, res) => {
   } else if (!passwordForm) {
     res.status(400).render("urls_error", { userDB: null, error: "You need to inform password!" });
   } else {
-    //const { userFromDb, error } = validateUser(emailForm, passwordForm, users);
     const { userDB, error } = validateUser(emailForm, passwordForm, users);
-
-    console.log("--->userDB inside /login:", userDB);
-    console.log("--->userDB.id inside /login:", userDB.id);
-
-    if(!userDB) {
+    if (!userDB) {
       res.status(403).render("urls_error", { userDB: null, error });
     } else {
-      // Replacing but cookie-session
-      // res.cookie('user_id', userFromDb.id);
-
       req.session['user_id'] = userDB.id;
-
-      console.log("--->req.session.user_id inside login:", req.session.user_id);
-
       const userUrlObj = getUserUrls(userDB.id, urlDatabase);
-      //templateVars = { urlDB: userUrlObj, userDB: userFromDb };
       templateVars = { urlDB: userUrlObj, userDB };
       res.render("urls_index", templateVars);
     }
@@ -225,7 +111,6 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  //res.clearCookie('user_id');
   req.session['user_id'] = null;
   res.render("urls_index", { urlDB: null, userDB: null});
 });
@@ -241,17 +126,8 @@ app.post("/register", (req, res) => {
     if (userDB) {
       res.render("urls_error", { userDB: null, error: `User ${email} already registered` });
     } else {
-      // Make this code a function addNewUser
-
       const { userDB, error } = addNewUser(id, email, password, users);
-
-      // password = bcrypt.hashSync(req.body.password, SALT_ROUND);
-      // users[id] = { id, email, password };
-      // // ---
-      // //res.cookie('user_id', id);
-
       req.session['user_id'] = id;
-
       const userUrlObj = getUserUrls(id,urlDatabase);
       const templateVars = { urlDB: userUrlObj, userDB : users[id] };
       res.render("urls_index", templateVars);
@@ -301,16 +177,13 @@ app.get("/urls", (req, res) => {
   const { userDB, error } = findUserById(userID, users);
  
   if (!userID) {
-    // if user doesn't exist send to regiters page
-    //const templateVars = { userDB: null, error : null };
-    //res.render("urls_login", templateVars);
     res.redirect("/login");
   } else {
     // Verify user exist based on the user_id cookie
     const { userDB, error } = findUserById(userID, users);
     
     if (error) {
-      templateVars = { userDB : null, error }
+      templateVars = { userDB : null, error };
       res.render("urls_error", templateVars);
     } else {
 
@@ -329,13 +202,10 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   userSessionID = req.session.user_id;
   if (!userSessionID) {
-    templateVars = { userDB: null, error: "User not logged"}
+    templateVars = { userDB: null, error: "User not logged"};
     res.render("urls_login", templateVars);
   } else {
     const { userDB, error } = findUserById(userSessionID, users);
-
-    console.log(">>>>>>>userDB inside /urls/new:", userDB);
-    console.log(">>>>>>>userDB['email'] inside /urls/new:", userDB['email']);
     templateVars = { userDB, error: null };
     res.render("urls_new", templateVars);
   }
--- helpers.js
+++ helpers.js
@@ -1,6 +1,8 @@
 const md5 = require('md5');
 const bcrypt = require('bcrypt');
 
+const SALT_ROUND      = 10;
+
 /// Helper Functions
 const generateRandomString = (text, length) => {
   length = !length ? 6 : length;
@@ -9,19 +11,19 @@ const generateRandomString = (text, length) => {
 };
 
 const findUserById = (id, users) => {
-    const userDB = Object.values(users).find(userObject => userObject.id === id);
+  const userDB = Object.values(users).find(userObject => userObject.id === id);
 
-    console.log("--->userDB inside findUserById:", userDB);
-    console.log("--->id inside findUserById:", id);
-    console.log("--->users inside findUserById:", users);
+  console.log("--->userDB inside findUserById:", userDB);
+  console.log("--->id inside findUserById:", id);
+  console.log("--->users inside findUserById:", users);
 
-    if (userDB) {
-      return { userDB, error: null };
-    } else {
-      // const user = null;
-      const userDB = null;
-      return { userDB, error: "User ID doesn't exist"}
-    }
+  if (userDB) {
+    return { userDB, error: null };
+  } else {
+    // const user = null;
+    const userDB = null;
+    return { userDB, error: "User ID doesn't exist"};
+  }
 
 };
 
@@ -31,11 +33,11 @@ const findUserByEmail = (email, userDB) => {
     return { user, error: null };
   } else {
     const user = null;
-    return { user, error: "User email doesn't exist"}
+    return { user, error: "User email doesn't exist"};
   }
 };
 
-function addNewUser(id, email, password, users) {
+const  addNewUser = (id, email, password, users) => {
   password = bcrypt.hashSync(password, SALT_ROUND);
   users[id] = { id, email, password };
   const userDB = users[id];
@@ -49,8 +51,8 @@ const validateUser = (email, password, users) => {
   } else if (!password) {
     return { userDB, error: "password empty" };
   } else if (!users) {
-    return { userDB, error: "database empty"} ;
-  };
+    return { userDB, error: "database empty"};
+  }
 
   userDB = Object.values(users).find(objectUser => objectUser.email === email);
   
@@ -58,7 +60,7 @@ const validateUser = (email, password, users) => {
 
   if (!userDB) {
     return { userDB , error: "User not found" };
-  } else { 
+  } else {
   
     const hash = userDB.password;
     
@@ -68,8 +70,8 @@ const validateUser = (email, password, users) => {
     if (userDB.email !== email) {
       return { userDB, error: "email not found!" };
     } else if (!bcrypt.compareSync(password, hash)) /* userFromDb.password !== password) */{
-      return { userDB, error: "wrong password" }; 
-    } 
+      return { userDB, error: "wrong password" };
+    }
     return { userDB, error: null };
   }
 };
@@ -77,15 +79,15 @@ const validateUser = (email, password, users) => {
 const getUserUrls = (id, urlDatabase) => {
   const userUrlObj = {};
 
-  console.log("---> urlDatabase:",urlDatabase)
+  console.log("---> urlDatabase:",urlDatabase);
 
   for (let url in urlDatabase) {
-    console.log("url in the loop:", url)
+    console.log("url in the loop:", url);
     console.log("urlDatabase[url] in the loop:", urlDatabase[url]);
     console.log("urlDatabase[url].userID in the loop:", urlDatabase[url].userID);
     console.log("userDB.id in the loop:", id);
 
-    if ( urlDatabase[url].userID == id) {
+    if (urlDatabase[url].userID === id) {
       console.log("--->", url);
       
       userUrlObj[url] =  urlDatabase[url];
