9cfd89b3c1ed9ab8fe29f811f104650458cef733
Sam <sribas@gmail.com>
2021-03-18 02:28:43 +0000

    Week3/Day3 - Adding user_id cookie, helper functions

--- express_server.js
+++ express_server.js
@@ -4,7 +4,12 @@ const md5 = require('md5');
 const ejs = require('ejs');
 const cookieParser = require('cookie-parser');
 const app = express();
+
 const PORT = 8080; // default port 8080
+const PASSWORD_LENGTH = 15;
+const USERID_LENGTH   = 6;
+const SHORTURL_LENGTH = 6;
+
 
 // My plan is to store this in a file when I get all functionalities
 // for this app implemented.
@@ -13,20 +18,54 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const userDB = [
-  { "samrib" : 
-    {
-      id: "samrib",
-      email: "sam@ribas.ca",
-      password: "sam"
-    }
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur",
+    urls: []
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk",
+    urls: []
   }
-];
+};
 
 function generateRandomString(text, length) {
   length = !length ? 6 : length;
-  const shortURLKey = md5(text).slice(0,length);
-  return shortURLKey;
+  const randomString = md5(text).slice(0,length);
+  return randomString;
+};
+
+function findUser(id, userDB) {
+  const user = Object.values(userDB).find(userObject => userObject.id === id);
+  return user;
+};
+
+function validateUser(email, password, userDB) {
+  userObj = null;
+  if (!email) {
+    return { userObj , error: "email empty" };
+  } else if (!password) {
+    return { userObj, error: "password empty" };
+  } else if (!userDB) {
+    return { userObj, error: "database empty"} ;
+  }
+
+  userFromDb = Object.values(userDB).find(objectUser => objectUser.email === email);
+  
+  if (!userFromDb) {
+    return { userObj , error: "User not found" };
+  }
+  
+  if (userFromDb.email !== email) {
+    return { userObj, error: "email not found!" };
+  } else if (userFromDb.password !== password) {
+    return { userObj, error: "wrong password" }; 
+  }
+  return { userFromDb, error: null };
 };
 
 app.set('view engine', 'ejs');
@@ -40,8 +79,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
-  // console.log("Edit body:", req.body);
-  // console.log("Edit params:", req.params);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username};
   res.render("urls_show", templateVars);
 });
@@ -52,38 +89,80 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  shortURLKey = generateRandomString(req.body.longURL);
+  shortURLKey = generateRandomString(req.body.longURL, SHORTURL_LENGTH);
   urlDatabase[shortURLKey] = req.body.longURL;
   res.redirect('/urls');
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect("/urls");
-  //res.send("Login page not implemented yet. Click <a href=\"/urls\">here</a> to return to the main page.");
+
+  const emailForm = req.body.email;
+  const passwordForm = req.body.password;
+
+  if (!emailForm || !passwordForm) {
+
+    res.status(400).send("You need to inform email and password!")
+  
+
+  } else {
+
+    const password = generateRandomString(passwordForm, PASSWORD_LENGTH);
+    const { userFromDb, error } = validateUser(emailForm, password, users);
+
+    console.log("userFromDb INSIDE login:", userFromDb);
+
+    if(!userFromDb) {
+      res.redirect("/register");
+    } else {
+      res.cookie('user_id', userFromDb.id);
+      templateVars = { urlDB: urlDatabase, userDB: userFromDb };
+      res.render("urls_login", templateVars);
+    }
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect("/urls");
+  res.clearCookie('user_id');
+  res.render("urls_index", { urlDB: null, userDB: null});
 });
 
 app.post("/register", (req, res) => {
-  templateVars = req.body;
+  const id = generateRandomString(req.body.email,USERID_LENGTH);
+  user = findUser(id, users);
+  email = req.body.email;
+
   // check if email was informed
-  if (templateVars.email) {
+  if (!user) {
+    // Verify if user informed the email on the form
+    if (email) {
+      password = generateRandomString(req.body.password, PASSWORD_LENGTH);
+      users[id] = { id, email, password, urls: [] };
+      res.cookie('user_id', id);
+      res.redirect("/urls");
+    } else {
+      res.redirect("/register");
+    }
+  } else {
+    
+    res.redirect("/urls");
     // check if the user already exist
     // if true 
-      // return message inform user already exist
+    // return message inform user already exist
     // if false
-      // add new user to the database
-    
+    // add new user to the databas 
   }
 })
 
 app.get("/register", (req, res) => {
-  console.log("Cookies /register", req.cookies);
-  res.render("urls_register", req.cookies);
+
+  // Find if user exists
+  const user = findUser(req.cookies.user_id, users);
+  //console.log(req.cookies);
+  if (user) {
+    res.render("urls_index", user);
+  } else {
+    res.render("urls_register", req.cookies);
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -96,8 +175,8 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/", (req, res) => {
   console.log("cookies at /", req.cookies);
-  username = req.cookies;
-  if (username) {
+  user_id = req.cookies.user_id;
+  if (user_id) {
     res.redirect('/urls');
   } else {
     res.render("urls_register");
@@ -109,13 +188,23 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  //console.log(req.cookies.username);
-  //console.log("cookies at /", req.cookies);
-  const templateVars = { urls: urlDatabase, username: req.cookies.username };
-  if (templateVars.username) {
-    res.render('urls_index.ejs', templateVars);
+
+  // Verify user exist based on the user_id cookie
+  const user = findUser(req.cookies.user_id, users);
+
+  console.log("Result of findUser inside app.get(/urls):", user);
+  const templateVars = { urlDB: urlDatabase, userDB: user };
+  console.log("------>templateVars before urls_index.ejs:", templateVars);
+
+  // Verify if user exist
+  if (user) {
+    // If user exist define templateVars with urlDB and user object
+    /// render urls_index passing templateVars
+    res.render('urls_index', templateVars);
   } else {
-    res.render("urls_register");
+    // if user doesn't exist send to regiters page
+    const templateVars = { urlDB: urlDatabase, userDB: null};
+    res.render("urls_index", templateVars);
   }
 
 });
@@ -129,10 +218,6 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
